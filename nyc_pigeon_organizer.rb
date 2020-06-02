affbbe0b6ace14e2943b3ff66ad7a0f892b5486a
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-02 21:47:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,43 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
-
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
-# key => :color
-# inner_key =>
-hash = {}
-pigeon_data.each do |key, val_hash|
-  val_hash.each do |inner_key, val_arr|
-      val_arr.each do |name|
-        if hash[name].nil? 
-          hash[name] = {}
-          hash[name][key] = []
+  hash = {}
+  pigeon_data.each do |key, val_hash|
+    val_hash.each do |inner_key, val_arr|
+        val_arr.each do |name|
+          if hash[name].nil? 
+            hash[name] = {}
+            hash[name][key] = []
+          end
+          
+          if hash[name][key].nil?
+            hash[name][key] = []
+          end
+          
+          hash[name][key] << inner_key.to_s
         end
-        
-        if hash[name][key].nil?
-          hash[name][key] = []
-        end
-        
-        hash[name][key] << inner_key.to_s
-      end
+    end
   end
-end
 
-puts hash
+  return hash
+end
