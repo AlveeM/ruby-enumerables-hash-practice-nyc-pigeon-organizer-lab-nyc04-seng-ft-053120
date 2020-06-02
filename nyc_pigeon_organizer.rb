14a6deaae9b4629052a83340f27e3331193f247f
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-02 21:48:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  
+  data.each do |key, val_hash|
+    val_hash.each do |inner_key, val_arr|
+        val_arr.each do |name|
+          if hash[name].nil? 
+            hash[name] = {}
+            hash[name][key] = []
+          end
+          
+          if hash[name][key].nil?
+            hash[name][key] = []
+          end
+          
+          hash[name][key] << inner_key.to_s
+        end
+    end
+  end
+
+  return hash
 end
