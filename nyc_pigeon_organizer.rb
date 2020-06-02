cbf12eb674bf4faebc05678b9421e84f3a9ad03f
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-02 21:28:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,7 @@ hash = {}
 pigeon_data.each do |key, val_hash|
   val_hash.each do |inner_key, val_arr|
       val_arr.each do |name|
-        (hash[name] != nil) ? hash[name] << 
+        (hash[name][key] != nil) ? (hash[name][key] << inner_key) : [inner_key]
       end
     #puts "#{inner_key}, #{val_arr}"
   end
