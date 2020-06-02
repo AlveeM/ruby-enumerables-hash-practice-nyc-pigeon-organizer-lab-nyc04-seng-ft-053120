dbcd1301c78fc79d0ce9df2a313ba020bd1f8335
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-02 21:42:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -27,6 +27,7 @@ pigeon_data.each do |key, val_hash|
   val_hash.each do |inner_key, val_arr|
       val_arr.each do |name|
         if hash[name].nil 
+          hash[name] = {}
           hash[name][key] = []
         end
         hash[name][key] << inner_key.to_s
